6f339cf525c0185b47a4548277afab5bc2b2a204
Sreeram.Chittela <sreerac@amazon.com>
2024-09-22 15:08:07 -0700

    Add a random priority now until priority feature is ready

--- src/main/java/com/lifebook/Lifebook/mappers/RequestMapper.java
+++ src/main/java/com/lifebook/Lifebook/mappers/RequestMapper.java
@@ -4,6 +4,7 @@ import com.lifebook.Lifebook.model.requests.CreateOrUpdateMemoryRequest;
 import com.lifebook.Lifebook.model.types.Memory;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -13,6 +14,16 @@ public interface RequestMapper {
 
     @Mapping(target = "modifiedDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "id", expression = "java(request.getId() != null ? request.getId() : java.util.UUID.randomUUID().toString())")
+    @Mapping(target = "priority", source = "request", qualifiedByName = "mapPriority")
     Memory toMemory(CreateOrUpdateMemoryRequest request);
+
+    @Named("mapPriority")
+    default int mapPriority(CreateOrUpdateMemoryRequest request) {
+        return request.getPriority() > 0 ? request.getPriority() : generateRandomPriority();
+    }
+
+    default int generateRandomPriority() {
+        return (int) (Math.random() * (1000 - 20 + 1)) + 20;
+    }
 
 }
